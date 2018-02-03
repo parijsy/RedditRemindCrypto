fc17c81ede015ee038ff6471454a9d1e71e3b3a8
VSP\parijsy <y.parijs@loket.nl>
2018-02-03 17:19:39 +0100

    Html updates for bootstrap4

--- RedditRemindCrypto/RedditRemindCrypto.Web/App_Start/BundleConfig.cs
+++ RedditRemindCrypto/RedditRemindCrypto.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace RedditRemindCrypto.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-{version}.slim.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -21,6 +22,7 @@ namespace RedditRemindCrypto.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/umd/popper.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
